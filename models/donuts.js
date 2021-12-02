3514de460ccbb97876d4c518f86115fd04da9d0d
S5454528 <S545528@NWMISSOURI.EDU>
2021-12-01 18:38:44 -0600

    added files

--- models/donuts.js
+++ models/donuts.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const donutSchema = mongoose.Schema({
     donut_type: String,
-    quantity: Number,
-    cost: Number
+    quantity: {type:Number, min : 4,max : 82, default : 0},
+    cost: {type:Number, min : 5,max : 80, default : 0}
 })
 
 module.exports = mongoose.model("Donut", donutSchema)
--- routes/donuts.js
+++ routes/donuts.js
@@ -2,6 +2,17 @@ var express = require('express');
 const donuts_controlers= require('../controllers/donuts'); 
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on 
+// or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    }
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 /* GET donuts */
 router.put('/donuts/:id', donuts_controlers.donuts_update_put);
 
@@ -17,11 +28,11 @@ router.get('/donuts/:id',donuts_controlers.donuts_detail);
 router.get('/detail', donuts_controlers.donuts_view_one_Page);  
 
 /* GET create donuts page */ 
-router.get('/create', donuts_controlers.donuts_create_Page); 
+router.get('/create',secured, donuts_controlers.donuts_create_Page); 
  
 /* GET create update page */ 
-router.get('/update', donuts_controlers.donuts_update_Page); 
+router.get('/update',secured,donuts_controlers.donuts_update_Page); 
 
 /* GET create donuts page */ 
-router.get('/delete', donuts_controlers.donuts_delete_Page);
+router.get('/delete',secured, donuts_controlers.donuts_delete_Page);
 module.exports = router;
